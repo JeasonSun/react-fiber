66c02da2e7619b81a571e71cab5e393338e9b723
mojie <jeason8023@gmail.com>
2020-08-05 21:29:08 +0800

    feat: 完成项目初始化

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const element = (
+  <div id='A1'>
+    <div id='B1'>
+      <div id='C1'></div>
+      <div id='C2'></div>
+    </div>
+    <div id='B2'></div>
+  </div>
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+console.log(element)
+
+ReactDOM.render(element, document.getElementById('root'))
